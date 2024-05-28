182d0751c679723d85c1fd34e3a00ced3d9e6c5c
selamsew Teshome <selamsewteshome19@gmail.com>
2024-05-28 12:09:50 -0800

    backend is  successfully built

--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -4,8 +4,13 @@ const mongoose = require("mongoose")
 const getBlog = async (req,res)=>{
     const {id} = req.params;
     if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status
+        return res.status(404).json({error:"no such blog"})
     } 
+    const blog = await Blog.findById(id)
+
+    if(!blog) return res.status(404).send({error:"no such blog"})
+
+    res.status(200).send(blog)
 }
 const createBlog = async (req,res)=>{
     const {title, description} = req.body;
@@ -19,6 +24,38 @@ const createBlog = async (req,res)=>{
     }
 }
 
+const updateBlog = async (req,res)=>{
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"no such blog"})
+    }
+  const blog = await Blog.findOneAndUpdate({_id:id},{
+    ...req.body
+  })
+
+  if(!blog){
+    res.status(400).send("no such blog")
+  }
+  res.status(200).send(blog)
+
+}
+
+const deleteBlog =async (req,res)=>{
+    const {id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"no such blog"})
+    }
+
+    const blog = await Blog.findOneAndDelete({_id:id})
+
+    if(!blog){
+        return res.status(400).json({error:"No such workout"})
+    }
+    res.status(200).send(blog)
+}
 module.exports = {
-    createBlog
+    createBlog,
+    getBlog,
+    updateBlog,
+    deleteBlog
 }
--- Backend/routes/blogRoutes.js
+++ Backend/routes/blogRoutes.js
@@ -1,23 +1,17 @@
 const express = require("express")
 const router = express.Router()
-const {createBlog} = require("../controllers/blogController")
+const {createBlog,getBlog,updateBlog,deleteBlog} = require("../controllers/blogController")
 
 router.get("/", (req, res) => {
   res.send("homepage");
 });
 
-router.get("/:id", (req, res) => {
-  res.send("specific id");
-});
+router.get("/:id", getBlog);
 
 router.post("/", createBlog);
 
-router.patch("/:id", (req, res) => {
-  res.send("patch request");
-});
+router.patch("/:id", updateBlog);
 
-router.delete("/:id", (req, res) => {
-  res.send("delete request");
-});
+router.delete("/:id", deleteBlog);
 
 module.exports = router
